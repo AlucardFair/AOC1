d3b0d48c5fa2b012befd9577d60202d324662af0
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-10-14 15:25:04 -0500

    Initial Commit. Started a fresh build for Project 4.

---
+++ Project-4/Project-4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Project-4
+//
+//  Created by Zachery Hernandez on 10/14/12.
+//  Copyright (c) 2012 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Project-4/Project-4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Project-4
+//
+//  Created by Zachery Hernandez on 10/14/12.
+//  Copyright (c) 2012 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
