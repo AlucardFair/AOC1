469d26e50865355b51422ddccdd4c93be3c0afb7
Zachery Hernandez <Zak@Zacherys-MacBook-Pro.local>
2012-10-14 16:10:50 -0500

    Initial Commit. Started a fresh build for Project 4.

---
+++ Project-4/Project-4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Project-4
+//
+//  Created by Zachery Hernandez on 10/14/12.
+//  Copyright (c) 2012 Zachery Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Project-4/Project-4/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  Project-4
+//
+//  Created by Zachery Hernandez on 10/14/12.
+//  Copyright (c) 2012 Zachery Hernandez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
